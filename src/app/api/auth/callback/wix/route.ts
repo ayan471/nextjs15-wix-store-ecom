0695a03f70cfdda4d6f2dbe2654ac16b2e9d0070
ayan471 <pakhiraayan@gmail.com>
2024-10-29 20:21:52 +0530

    fixinng bbugs

--- src/app/api/auth/callback/wix/route.ts
+++ src/app/api/auth/callback/wix/route.ts
@@ -1,5 +1,6 @@
 import { WIX_OAUTH_DATA_COOKIE, WIX_SESSION_COOKIE } from "@/lib/constants";
 import { getWixServerClient } from "@/lib/wix-client-server";
+
 import { OauthData } from "@wix/sdk";
 import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
@@ -17,9 +18,11 @@ export async function GET(req: NextRequest) {
   const oAuthData: OauthData = JSON.parse(
     cookies().get(WIX_OAUTH_DATA_COOKIE)?.value || "{}",
   );
+
   if (!code || !state || !oAuthData) {
-    return new Response("Invalid Request", { status: 400 });
+    return new Response("Invalid request", { status: 400 });
   }
+
   const wixClient = getWixServerClient();
 
   const memberTokens = await wixClient.auth.getMemberTokens(
@@ -27,13 +30,17 @@ export async function GET(req: NextRequest) {
     state,
     oAuthData,
   );
+
   cookies().delete(WIX_OAUTH_DATA_COOKIE);
   cookies().set(WIX_SESSION_COOKIE, JSON.stringify(memberTokens), {
     maxAge: 60 * 60 * 24 * 14,
     secure: process.env.NODE_ENV === "production",
   });
+
   return new Response(null, {
     status: 302,
-    headers: { Location: oAuthData.originalUri || "/" },
+    headers: {
+      Location: oAuthData.originalUri || "/",
+    },
   });
 }
